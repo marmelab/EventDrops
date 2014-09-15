d301b0056e3cd7010e01fd711d54bbf7bc15d9df
ThieryMichel <thiery@marmelab.com>
2014-09-15 14:56:16 +0200

    add timeline chart to d3 that append a simple svg element

--- src/timeline.js
+++ src/timeline.js
@@ -1,3 +1,34 @@
 (function () {
+  function configurable(targetFunction, config) {
+    for (var item in config) {
+      (function(item) {
+        targetFunction[item] = function(value) {
+          if (!arguments.length) return config[item];
+          config[item] = value;
 
+          return targetFunction;
+        };
+      })(item); // for doesn't create a closure, forcing it
+    }
+  };
+
+  d3.timeline = function (element, configData) {
+    var svg;
+    var config = {
+      start: new Date(0),
+      end: new Date()
+    };
+
+    var timeline = function () {
+      if (!svg) {
+        svg = d3.select(element)
+          .append('svg')
+        ;
+      }
+    };
+
+    configurable(timeline, config);
+
+    return timeline;
+  };
 })(d3);
--- test/timeline.js
+++ test/timeline.js
@@ -1,7 +1,38 @@
 "use strict";
 
-describe('timeline', function () {
-  it('should test', function () {
-    expect(true).toBe(true);
+describe('d3', function () {
+
+  it('should add a timeline function to d3 that return a function', function () {
+    expect(typeof d3.timeline).toBe('function');
+    expect(typeof d3.timeline()).toBe('function');
   });
+
+  describe('timeline', function () {
+    var timeline;
+    var element;
+
+    beforeEach(function () {
+      element = document.createElement('div')
+      timeline = d3.timeline(element);
+    });
+
+    it ('should append only one svg element to the given element when called', function () {
+      timeline();
+      var svgs = element.getElementsByTagName('svg');
+      expect(svgs.length).toBe(1);
+
+      timeline();
+      var svgs = element.getElementsByTagName('svg');
+      expect(svgs.length).toBe(1);
+    });
+
+    it ('should have a configurable start', function () {
+      expect(typeof timeline.start).toBe('function');
+      expect(timeline.start().getTime()).toBe((new Date(0)).getTime());
+
+      timeline.start(new Date(100));
+      expect(timeline.start().getTime()).toBe((new Date(100)).getTime());
+    });
+  });
+
 });
