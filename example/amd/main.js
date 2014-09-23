575f438d30e7b1b2f9c434187f7d23bb86a3acc8
Francois Zaninotto <fzaninotto@gmail.com>
2014-09-23 14:43:31 +0200

    Fix margin, delimiter, and codehints

--- example/amd/main.js
+++ example/amd/main.js
@@ -13,40 +13,40 @@ require.config({
 });
 
 require(['d3', 'd3.chart.eventDrops'], function(d3) {
-    var body = document.getElementsByTagName('body')[0];
 
-    var names = ["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur", "Adipisicing", "elit", "Eiusmod tempor", "Incididunt"];
+    // create dataset
     var data = [];
-    var endTime = Date.now();
+    var names = ["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur", "Adipisicing", "elit", "Eiusmod tempor", "Incididunt"];
     var month = 30 * 24 * 60 * 60 * 1000;
     var startTime = endTime - 6 * month;
-
+    var endTime = Date.now();
     function createEvent (name, maxNbEvents) {
         maxNbEvents = maxNbEvents | 200;
         var event = {
             name: name,
             dates: []
         };
-
         // add up to 200 events
         var max =  Math.floor(Math.random() * maxNbEvents);
         for (var j = 0; j < max; j++) {
             var time = (Math.random() * (endTime - startTime)) + startTime;
             event.dates.push(new Date(time));
         }
-
         return event;
     }
-
     for (var i = 0; i < 10; i++) {
         data.push(createEvent(names[i]));
     }
 
-    var graph = d3.chart.eventDrops({
-        start: new Date(startTime),
-        end: new Date(endTime)
-    });
+    // create chart function
+    var eventDropsChart = d3.chart.eventDrops()
+        .start(new Date(startTime))
+        .end(new Date(endTime));
 
+    // bind data with DOM
+    var body = document.getElementsByTagName('body')[0];
     var element = d3.select(body).append('div').datum(data);
-    graph(element);
+
+    // draw the chart
+    eventDropsChart(element);
 });
