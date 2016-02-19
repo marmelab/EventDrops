c91cc99a8708e72929ae3a1f20dc38a32e629f43
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-02-19 07:37:30 +0100

    Use Firefox instead of PhantomJS for tests

--- test/karma/bind.shim.js
+++
@@ -1,7 +0,0 @@
-// PhantomJS doesn't support bind yet
-Function.prototype.bind = Function.prototype.bind || function (thisp) {
-    var fn = this;
-    return function () {
-        return fn.apply(thisp, arguments);
-    };
-};
