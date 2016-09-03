78b2721e548c60f9b65acfbe841dbec639a848db
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-09-03 22:44:48 +0200

    Prepare a (buggy) script to import data from GitHub API

---
+++ demo/bin/updateData.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+require('isomorphic-fetch');
+
+const repositories = [
+    { name: 'ng-admin', identifier: 'marmelab/ng-admin' },
+    { name: 'EventDrops', identifier: 'marmelab/EventDrops' },
+    { name: 'javascript-boilerplate', identifier: 'marmelab/javascript-boilerplate' },
+];
+
+const promises = repositories.map(repository => {
+    return fetch('https://api.github.com/repos/marmelab/ng-admin/commits')
+        .then(response => {
+            if (response.status >= 400) {
+                console.error(`Unable to retrieve commits for "${repository.name}" repository (response ${response.status}).`);
+            }
+
+            return response.json();
+        })
+        .then(commits => {
+            repository.commits = commits;
+        })
+        .catch(console.err);
+});
+
+Promise.all(promises)
+    .then(() => {
+        fs.writeFile(__dirname + '/../data.json', JSON.stringify(repositories), err => {
+            if (err) {
+                console.error(`Unable to write "data.json" file.`);
+                return;
+            }
+
+            console.log('Successfully updated data fixtures.');
+        });
+    });
--- demo/demo.js
+++ demo/demo.js
@@ -1,20 +1,18 @@
-const repositories = [{
-    name: 'EventDrops',
-    data: require('json!./data.json'),
-}, {
-    name: 'ng-admin',
-    data: require('json!./data.json'),
-}];
+const repositories = require('json!./data.json');
 
 const colors = d3.scale.category10();
-const [start, end] = d3.extent(repositories[0].data, d => d.commit.author.date)
+const [start, end] = d3.extent(repositories[0].commits, d => d.commit.author.date)
     .map(d => new Date(d));
 
 const chart = d3.chart.eventDrops()
     .start(start)
     .end(end)
-    .eventLineColor(() => colors(0))
+    .eventLineColor((d, i) => colors(i))
     .date(d => new Date(d.commit.author.date));
 
-const element = d3.select('#eventdrops-demo').datum(repositories);
+const element = d3.select('#eventdrops-demo').datum(repositories.map(repository => ({
+    name: repository.name,
+    data: repository.commits,
+})));
+
 chart(element);
