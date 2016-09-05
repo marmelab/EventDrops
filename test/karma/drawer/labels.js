76fef7a0707f8451afe5c93f97e83bb0ffc33dd2
Tristan Newmann <tnew2294@gmail.com>
2016-09-05 10:39:29 +1000

    Add unit test for label event count (complex data)

--- test/karma/drawer/labels.js
+++ test/karma/drawer/labels.js
@@ -17,6 +17,7 @@ describe('Labels drawer', () => {
     let container;
 
     beforeEach(() => {
+        d3.select('body').html('');
         svg = d3.select('body').append('svg');
         svg.append('g').classed('extremum', true);
 
@@ -30,5 +31,19 @@ describe('Labels drawer', () => {
         scales.x.domain(domain);
         labels(container, scales, config)(data);
         expect(+svg.select('.label').attr('x')).toBe(50);
+    });
+
+    it('should show label count with complex data', () => {
+        const config =  {
+            date: d => d.date
+        };
+        const complexData = [{ name: 'foo', data: [{ test: 'bar', date: new Date('2014-02-01') }, { test: 'baz', date: new Date('2014-03-01') }]}];
+
+        scales.x.domain(domain);
+        labels(container, scales, config)(complexData);
+
+        //expect(div.querySelector('.extremum .minimum').textContent).toBe('25 January 2010');
+        expect(document.querySelector('.label').textContent).toBe('foo (2)');
+
     });
 });
