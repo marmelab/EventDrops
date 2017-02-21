5ec00bea8fbd85f99da0ac6e9bfb64001a4d497a
osadan <sadanoh@gmail.com>
2017-02-21 16:26:45 +0200

    the first change d3.time.format to d3.timeParse

--- webpack/entries.js
+++ webpack/entries.js
@@ -11,7 +11,7 @@ module.exports = function (production) {
     if (!production) {
         /* D3 is defined as an external, but we need it for the demo. So, let's
         include it using the full path to trick Webpack. */
-        entries['demo-d3'] = path.join(__dirname, '../node_modules/d3/d3.min.js');
+        //entries['demo-d3'] = path.join(__dirname, '../node_modules/d3/build/d3.min.js');
         entries.demo = [
             path.join(__dirname, '../demo/demo.js'),
             path.join(__dirname, '../demo/demo.css'),
