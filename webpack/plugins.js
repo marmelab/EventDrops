0c62c3b7bebed89e1ef72c2b752598c9f7b91cff
osadan <sadanoh@gmail.com>
2017-03-07 10:46:25 +0200

    add d3.min to dist folder

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,5 +1,6 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ProvidePlugin = require('webpack').ProvidePlugin;
 
 var path = require('path');
@@ -14,7 +15,12 @@ module.exports = function (production) {
             template: path.join(__dirname, '../demo/index-dev.html'),
             hash: true
         }));
+
+        plugins.push(new CopyWebpackPlugin([
+                {from : path.join(__dirname,'../node_modules/d3/build/d3.min.js')}
+            ]))
     }
+
 
     return plugins;
 };
