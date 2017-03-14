709848ad7524170b65fba75e22b0bde94fefbd1a
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-03-14 18:56:45 +0100

    Prettify demo files

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,28 @@
 const md5 = require('./md5');
 const repositories = require('json!./data.json');
-import * as  d3 from  'd3/build/d3';
+import * as d3 from 'd3/build/d3';
 
 import eventDrops from '../lib/eventDrops';
 
 const colors = d3.schemeCategory10;
-const gravatar = email => `https://www.gravatar.com/avatar/${md5(email.trim().toLowerCase())}`;
+const gravatar = email =>
+    `https://www.gravatar.com/avatar/${md5(email.trim().toLowerCase())}`;
 
 // September 4 1986 8:30 PM
 const humanizeDate = date => {
     const monthNames = [
-        'Jan.', 'Feb.', 'March', 'Apr.', 'May', 'June',
-        'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.',
+        'Jan.',
+        'Feb.',
+        'March',
+        'Apr.',
+        'May',
+        'June',
+        'Jul.',
+        'Aug.',
+        'Sept.',
+        'Oct.',
+        'Nov.',
+        'Dec.',
     ];
 
     return `
@@ -26,34 +37,34 @@ const TOOLTIP_WIDTH = 30; // in rem
 // we're gonna create a tooltip per drop to prevent from transition issues
 const showTooltip = commit => {
     d3.select('body').selectAll('.tooltip').remove();
-    
-    const tooltip = d3.select('body')
+
+    const tooltip = d3
+        .select('body')
         .append('div')
         .attr('class', 'tooltip')
         .style('opacity', 0); // hide it by default
-        
-    const t = d3.transition()
-        .duration(750)
-        .ease(d3.easeLinear);
-        
-    tooltip.transition(t)
-        .on('start',()=> {
-            d3.select('.tooltip')
-              .style('display','block');
+
+    const t = d3.transition().duration(750).ease(d3.easeLinear);
+
+    tooltip
+        .transition(t)
+        .on('start', () => {
+            d3.select('.tooltip').style('display', 'block');
         })
-        .style('opacity',1);
-    
+        .style('opacity', 1);
+
     const rightOrLeftLimit = FONT_SIZE * TOOLTIP_WIDTH;
-    
+
     const direction = d3.event.pageX > rightOrLeftLimit ? 'right' : 'left';
 
     const ARROW_MARGIN = 1.65;
     const ARROW_WIDTH = FONT_SIZE;
-    const left = direction === 'right' ?
-        d3.event.pageX - rightOrLeftLimit :
-        d3.event.pageX - ARROW_MARGIN * FONT_SIZE - ARROW_WIDTH / 2;
-        
-    tooltip.html(`
+    const left = direction === 'right'
+        ? d3.event.pageX - rightOrLeftLimit
+        : d3.event.pageX - ARROW_MARGIN * FONT_SIZE - ARROW_WIDTH / 2;
+
+    tooltip.html(
+        `
             <div class="commit">
                 <img class="avatar" src="${gravatar(commit.author.email)}" alt="${commit.author.name}" title="${commit.author.name}" />
                 <div class="content">
@@ -65,18 +76,21 @@ const showTooltip = commit => {
                     </p>
                 </div>
             </div>
-        `);
-        tooltip.style('left', `${left}px`)
-            .style('top', (d3.event.pageY + 16) + 'px')
-            .classed(direction, true);
+        `
+    );
+    tooltip
+        .style('left', `${left}px`)
+        .style('top', d3.event.pageY + 16 + 'px')
+        .classed(direction, true);
 };
 
 const hideTooltip = () => {
-     const t = d3.transition()
-                 .duration(1000);
-    
-    d3.select('.tooltip').transition(t)
-        .on('end',this.remove)
+    const t = d3.transition().duration(1000);
+
+    d3
+        .select('.tooltip')
+        .transition(t)
+        .on('end', this.remove)
         .style('opacity', 0);
 };
 
@@ -88,9 +102,11 @@ const chart = eventDrops()
     .mouseover(showTooltip)
     .mouseout(hideTooltip);
 
-const element = d3.select('#eventdrops-demo').datum(repositories.map(repository => ({
-    name: repository.name,
-    data: repository.commits,
-})));
+const element = d3.select('#eventdrops-demo').datum(
+    repositories.map(repository => ({
+        name: repository.name,
+        data: repository.commits,
+    }))
+);
 
 chart(element);
