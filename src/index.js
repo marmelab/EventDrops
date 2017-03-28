3839f52200be55a8139a06321cc3bfcde283f171
osadan <sadanoh@gmail.com>
2017-03-28 16:08:25 +0300

    those typos...

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import drawer from './drawer';
 import zoom from './zoom';
 
 function eventDrops(config = {}) {
-    const finalConfiguration = Object.assing({}, defaultConfig, config );
+    const finalConfiguration = Object.assign({}, defaultConfig, config );
 
     const yScale = data => {
         return d3
