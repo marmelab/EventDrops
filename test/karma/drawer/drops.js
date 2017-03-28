451e1a9e579ff215dac2b4aef672c0c2487e9c5f
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-03-28 17:48:54 +0200

    Add and fix some tests

--- test/karma/drawer/drops.js
+++ test/karma/drawer/drops.js
@@ -66,7 +66,50 @@ describe('Drops drawer', () => {
         d3.select(wrapper).datum(data).call(chart);
 
         const drop = d3.select('.drop');
-        expect(+drop.attr('cx')).toBe(50);
+        expect(+drop.attr('cx')).toBe(-60);
+    });
+
+    describe('.drop-line', () => {
+        it('should set correct width based on given scale', () => {
+            const data = [{ name: 'foo', data: [new Date('2014-04-03')] }];
+
+            const chart = eventDrops()
+                .labelsWidth(0)
+                .labelsRightMargin(0)
+                .start(new Date('2014-04-02'))
+                .end(new Date('2014-04-04'));
+
+            d3.select(wrapper).datum(data).call(chart);
+
+            const dropLine = d3.select('.drop-line');
+            expect(+dropLine.attr('width')).toBe(100);
+        });
+
+        it('should position each line at correct vertical position depending of current serie', () => {
+            const data = [
+                { name: 'foo', data: [new Date('2014-04-03')] },
+                { name: 'bar', data: [new Date('2014-05-03')] },
+                { name: 'quz', data: [new Date('2014-06-03')] },
+            ];
+
+            const chart = eventDrops()
+                .start(new Date('2014-04-02'))
+                .end(new Date('2014-04-04'));
+
+            d3.select(wrapper).datum(data).call(chart);
+
+            const dropLines = document.querySelectorAll('.drop-line');
+            const topTranslate = [...dropLines].map(
+                dropLine =>
+                    dropLine
+                        .getAttribute('transform')
+                        .split(',')
+                        .pop()
+                        .split(')')[0]
+            );
+
+            expect(topTranslate.map(t => +t)).toEqual([0, 40, 80]);
+        });
     });
 
     afterEach(() => {
