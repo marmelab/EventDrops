22ed655c5360e873628d5fa23dd7271a1a88d4ec
Florian FERBACH <florian@marmelab.com>
2018-07-11 11:54:09 +0200

    Add @TODO for date formazt

--- src/isAfter.js
+++ src/isAfter.js
@@ -2,5 +2,7 @@ import dateIsAfter from 'date-fns/is_after';
 
 export const isAfter = (date, dateBounds) => {
     const endingDate = Math.max(...dateBounds);
+
+    // @TODO: remove the `new Date()` constructor in the next major version: we need to force it at configuration level.
     return dateIsAfter(new Date(date), endingDate);
 };
